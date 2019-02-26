6386e2d26910f6e08304da9a512e959502690792
Phrasefable <12666934+phrasefable@users.noreply.github.com>
2019-02-27 02:34:40 +1300

    Remove debug code

--- AggressiveAcorns/AggressiveAcorns.cs
+++ AggressiveAcorns/AggressiveAcorns.cs
@@ -16,62 +16,37 @@ namespace AggressiveAcorns
         {
             _config = helper.ReadConfig<ModConfig>();
 
-            FileLog.Reset();
-            HarmonyInstance.DEBUG = true;
             var harmony = HarmonyInstance.Create(ModManifest.UniqueID);
 
-            var updatepatch = new Patch_Tree_DayUpdate();
-            _patches.Add(updatepatch);
-            if (_config.bPreventScythe)
-            {
-                _patches.Add(new Patch_Tree_PerformToolAction());
-            }
+            LoadPatches();
+            ApplyPatches(harmony);
+            
+            
+            Helper.Events.GameLoop.GameLaunched += ValidatePatches;
+        }
 
+        private void ApplyPatches(HarmonyInstance harmony)
+        {
             foreach (var patch in _patches)
             {
                 patch.ApplyPatch(harmony);
             }
-            
-            
-            Helper.Events.GameLoop.GameLaunched += ValidatePatches;
-            HarmonyInstance.DEBUG = false;
-            Monitor.Log("Loaded aggressive acorns.");
         }
 
-        private void LogPatches(HarmonyInstance harmony)
+        private void LoadPatches()
         {
-            foreach (var patchedMethod in harmony.GetPatchedMethods())
+            _patches.Add(new Patch_Tree_DayUpdate());
+            if (_config.bPreventScythe)
             {
-                Monitor.Log($"Found patched method: {patchedMethod.DeclaringType}::{patchedMethod.Name}");
-                var patches = harmony.GetPatchInfo(patchedMethod);
-                Monitor.Log("  Has prefixes:");
-                foreach (var prefix in patches.Prefixes)
-                {
-                    Monitor.Log($"    Owner: {prefix.owner}; method: {prefix.patch.Name}");
-                }
-
-                Monitor.Log("  Has postfixes:");
-                foreach (var postfix in patches.Postfixes)
-                {
-                    Monitor.Log($"    Owner: {postfix.owner}; method: {postfix.patch.Name}");
-                }
-
-                Monitor.Log("  Has transpilers:");
-                foreach (var trans in patches.Transpilers)
-                {
-                    Monitor.Log($"    Owner: {trans.owner}; method: {trans.patch.Name}");
-                }
+                _patches.Add(new Patch_Tree_PerformToolAction());
             }
         }
 
         private void ValidatePatches(object sender, GameLaunchedEventArgs e)
         {
             var harmony = HarmonyInstance.Create(ModManifest.UniqueID);
-            LogPatches(harmony);
-            Monitor.Log("Validating patches.");
             foreach (var patch in _patches)
             {
-                Monitor.Log($"Validating patch {patch.GetType()}");
                 if (!patch.IsValid(harmony, out var errors))
                 {
                     Monitor.Log(errors, LogLevel.Error);
