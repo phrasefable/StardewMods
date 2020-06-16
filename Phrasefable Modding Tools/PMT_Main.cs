c324cb46e755f2e0c0c3fba0d65bc70cbbb42468
Phrasefable <12666934+phrasefable@users.noreply.github.com>
2020-06-16 14:46:18 +1200

    PMT: Add command for listing all GameLocations

--- Phrasefable Modding Tools/PMT_Main.cs
+++ Phrasefable Modding Tools/PMT_Main.cs
@@ -1,5 +1,6 @@
 ﻿using JetBrains.Annotations;
 using StardewModdingAPI;
+using StardewValley;
 
 namespace Phrasefable.StardewMods.ModdingTools
 {
@@ -15,6 +16,32 @@ namespace Phrasefable.StardewMods.ModdingTools
             SetUp_EventLogging();
 
             SetUp_Clear();
+
+            SetUp_Misc();
+        }
+
+        private void SetUp_Misc()
+        {
+            Helper.ConsoleCommands.Add(
+                "list_game_locations",
+                "Lists all game locations",
+                ListGameLocations
+            );
+        }
+
+        private void ListGameLocations(string arg1, string[] arg2)
+        {
+            if (!Context.IsWorldReady)
+            {
+                Monitor.Log("World not ready.", LogLevel.Info);
+                return;
+            }
+
+            Monitor.Log("(Currently loaded) Game Locations:", LogLevel.Info);
+            foreach (GameLocation location in Common.Utilities.GetLocations(Helper))
+            {
+                Monitor.Log($"  name={location.Name}", LogLevel.Info);
+            }
         }
     }
 }
