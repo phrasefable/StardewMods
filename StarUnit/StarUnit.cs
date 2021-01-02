fae5e0e841fb321dd511eda7a42fa950519cdde3
Phrasefable <12666934+phrasefable@users.noreply.github.com>
2021-01-02 22:35:24 +1300

    SU: (WIPv6) Set up new project

---
+++ StarUnit/StarUnit.cs
@@ -0,0 +1,12 @@
+using StardewModdingAPI;
+
+namespace Phrasefable.StardewMods.StarUnit
+{
+    public class StarUnit : Mod
+    {
+        public override void Entry(IModHelper helper)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
