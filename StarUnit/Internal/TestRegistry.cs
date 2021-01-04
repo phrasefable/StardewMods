f5cdf2fb7c4b1d96871bcd33b077860e3f9bad78
Phrasefable <12666934+phrasefable@users.noreply.github.com>
2021-01-04 19:24:42 +1300

    SU: (WIPv6) Create test registry

---
+++ StarUnit/Internal/TestRegistry.cs
@@ -0,0 +1,83 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Phrasefable.StardewMods.StarUnit.Framework.Model;
+
+namespace Phrasefable.StardewMods.StarUnit.Internal
+{
+    public class TestRegistry
+    {
+        private readonly IDictionary<string, TestRoot> _testRoots = new Dictionary<string, TestRoot>();
+
+        private readonly Action<string> _writer;
+        private readonly Action<string> _errorWriter;
+
+        public IEnumerable<ITestSuite> TestRoots => this._testRoots.Values.Select(root => root.TestSuite);
+
+        public TestRegistry(Action<string> writer, Action<string> errorWriter)
+        {
+            this._writer = writer;
+            this._errorWriter = errorWriter;
+        }
+
+        private static IEnumerable<string> FindDuplicates(IEnumerable<string> list1, IEnumerable<string> list2)
+        {
+            ICollection<string> uniqueStrings = new HashSet<string>(list1);
+            foreach (string s in list2)
+            {
+                if (uniqueStrings.Contains(s))
+                {
+                    yield return s;
+                }
+
+                uniqueStrings.Add(s);
+            }
+        }
+
+
+        public void Register(string modId, params ITraversable[] testNodes)
+        {
+            if (!this._testRoots.TryGetValue(modId, out TestRoot root))
+            {
+                root = new TestRoot(modId);
+                this._testRoots[modId] = root;
+            }
+
+            IEnumerable<string> duplicates = TestRegistry.FindDuplicates(
+                    root.Children.Select(c => c.Key),
+                    testNodes.Select(c => c.Key)
+                )
+                .ToArray();
+
+            if (duplicates.Any())
+            {
+                this._errorWriter(
+                    $"Failed to register {testNodes.Count()} items at '{modId}' due to {duplicates.Count()} duplicates:"
+                );
+                this._errorWriter("    " + string.Join(" ", duplicates));
+                return;
+            }
+
+            foreach (ITraversable node in testNodes)
+            {
+                root.Children.Add(node);
+            }
+
+            this._writer($"Registered {testNodes.Count()} test node(s) to root '{modId}'.");
+        }
+
+        // ============================================================================================================
+
+        private class TestRoot
+        {
+            public ITestSuite TestSuite { get; }
+            public IList<ITraversable> Children { get; }
+
+            public TestRoot(string key)
+            {
+                this.Children = new List<ITraversable>();
+                this.TestSuite = new TestSuite {Key = key, Children = this.Children};
+            }
+        }
+    }
+}
--- StarUnit/StarUnit.cs
+++ StarUnit/StarUnit.cs
@@ -1,8 +1,4 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
 using JetBrains.Annotations;
-using Phrasefable.StardewMods.StarUnit.Framework.Model;
 using Phrasefable.StardewMods.StarUnit.Internal;
 using Phrasefable.StardewMods.StarUnit.Internal.Builders;
 using StardewModdingAPI;
@@ -12,45 +8,24 @@ namespace Phrasefable.StardewMods.StarUnit
     [UsedImplicitly]
     public class StarUnit : Mod
     {
-        private readonly IDictionary<string, IList<ITraversable>> TestNodes =
-            new Dictionary<string, IList<ITraversable>>();
+        private TestRegistry _tests;
 
-        public override void Entry(IModHelper helper) { }
+
+        public override void Entry(IModHelper helper)
+        {
+            this._tests = new TestRegistry(
+                s => this.Monitor.Log(s, LogLevel.Trace),
+                s => this.Monitor.Log(s, LogLevel.Error)
+            );
+        }
+
 
         public override object GetApi()
         {
-            return new StarUnitApi(this.Register)
+            return new StarUnitApi(this._tests.Register)
             {
                 BuilderFactory = new BuilderFactory()
             };
-        }
-
-        private void Register(string modId, params ITraversable[] testNodes)
-        {
-            this.TestNodes.TryGetValue(modId, out IList<ITraversable> rootNodes);
-            if (rootNodes == null)
-            {
-                rootNodes = new List<ITraversable>();
-                this.TestNodes[modId] = rootNodes;
-            }
-
-            ICollection<string> rootKeys = new HashSet<string>(rootNodes.Select(node => node.Key).ToArray());
-
-            foreach (ITraversable newRoot in testNodes)
-            {
-                if (rootKeys.Contains(newRoot.Key))
-                {
-                    throw new ArgumentException(
-                        "Cannot register tests or test suites at root level with duplicate keys.",
-                        nameof(testNodes)
-                    );
-                }
-
-                rootKeys.Add(newRoot.Key);
-                rootNodes.Add(newRoot);
-            }
-
-            Monitor.Log($"Registered {rootNodes.Count} root test-node(s) at '{modId}'.");
         }
     }
 }
