ee77722f0056266749741401d232c35be2f2b400
Phrasefable <12666934+phrasefable@users.noreply.github.com>
2021-02-13 17:45:33 +1300

    AA: Make ConfigAdapter a proper wrapper

--- AggressiveAcorns.InGameTest/MutableConfigAdaptor.cs
+++ AggressiveAcorns.InGameTest/MutableConfigAdaptor.cs
@@ -1,81 +1,127 @@
 using System;
 using System.Diagnostics.CodeAnalysis;
+using JetBrains.Annotations;
 
 namespace Phrasefable.StardewMods.AggressiveAcorns.InGameTest
 {
+    [SuppressMessage("ReSharper", "UnusedMember.Global")]
     [SuppressMessage("ReSharper", "AutoPropertyCanBeMadeGetOnly.Global")]
     [SuppressMessage("ReSharper", "MemberCanBePrivate.Global")]
-    [SuppressMessage("ReSharper", "UnusedAutoPropertyAccessor.Global")]
-    [SuppressMessage("ReSharper", "UnusedMember.Global")]
-    public class MutableConfigAdaptor : IConfigAdapter, IModConfig
+    public class MutableConfigAdaptor : IConfigAdapter
     {
-        private int _maxPassableGrowthStage;
+        private readonly ModConfig _config;
+        private readonly IConfigAdapter _adapter;
+
 
         public MutableConfigAdaptor()
         {
-            var config = new ModConfig();
+            this._config = new ModConfig();
+            this._adapter = new ConfigAdapter(this._config);
 
-            this.ProtectFromMelee = config.PreventScythe;
-            this.SeedsReplaceGrass = config.SeedsReplaceGrass;
-            this.MaxShadedGrowthStage = config.MaxShadedGrowthStage;
-            this.MaxPassableGrowthStage = config.MaxPassibleGrowthStage;
-            this.DoGrowInWinter = config.DoGrowInWinter;
-            this.DoTappedSpread = config.DoTappedSpread;
-            this.DoSpreadInWinter = config.DoSpreadInWinter;
-            this.DoGrowInstantly = config.DoGrowInstantly;
-            this.DoSeedsPersist = config.DoSeedsPersist;
-            this.DoMushroomTreesHibernate = config.DoMushroomTreesHibernate;
-            this.DoMushroomTreesRegrow = config.DoMushroomTreesRegrow;
-
-            this.DailySpreadChance = config.DailySpreadChance;
-            this.DailyGrowthChance = config.DailyGrowthChance;
-            this.DailySeedChance = config.DailySeedChance;
+            this.SpreadRoller = () => this._adapter.RollForSpread;
+            this.GrowthRoller = () => this._adapter.RollForGrowth;
+            this.SeedRoller = () => this._adapter.RollForSeed;
+            this.MushroomRegrowthRoller = () => this._adapter.RollForMushroomRegrowth;
         }
 
-        public bool ProtectFromMelee { get; set; }
-        public bool PreventScythe { get; set; }
-        public bool SeedsReplaceGrass { get; set; }
-        public int MaxShadedGrowthStage { get; set; }
-        public double DailyGrowthChance { get; set; }
-        public bool DoGrowInWinter { get; set; }
-        public double DailySpreadChance { get; set; }
-        public bool DoTappedSpread { get; set; }
-        public bool DoSpreadInWinter { get; set; }
-        public bool DoGrowInstantly { get; set; }
-        public bool DoSeedsPersist { get; set; }
-        public double DailySeedChance { get; set; }
-        public bool DoMushroomTreesHibernate { get; set; }
-        public bool DoMushroomTreesRegrow { get; set; }
 
-        // ReSharper disable once ConvertToAutoPropertyWhenPossible
-        public int MaxPassibleGrowthStage
+        // ======= Overridable function members ========================================================================
+
+        [NotNull] public Func<bool> SpreadRoller { private get; set; }
+        [NotNull] public Func<bool> GrowthRoller { private get; set; }
+        [NotNull] public Func<bool> SeedRoller { private get; set; }
+        [NotNull] public Func<bool> MushroomRegrowthRoller { private get; set; }
+
+
+        // ======= IConfigAdapter members ==============================================================================
+
+        public bool ProtectFromMelee
         {
-            get => this._maxPassableGrowthStage;
-            set => this._maxPassableGrowthStage = value;
+            get => this._adapter.ProtectFromMelee;
+            set => this._config.PreventScythe = value;
         }
 
-        // ReSharper disable once ConvertToAutoPropertyWhenPossible
+        public bool SeedsReplaceGrass
+        {
+            get => this._adapter.SeedsReplaceGrass;
+            set => this._config.SeedsReplaceGrass = value;
+        }
+
+        public int MaxShadedGrowthStage
+        {
+            get => this._adapter.MaxShadedGrowthStage;
+            set => this._config.MaxShadedGrowthStage = value;
+        }
+
+        public bool DoGrowInWinter
+        {
+            get => this._adapter.DoGrowInWinter;
+            set => this._config.DoGrowInWinter = value;
+        }
+
+        public bool DoTappedSpread
+        {
+            get => this._adapter.DoTappedSpread;
+            set => this._config.DoTappedSpread = value;
+        }
+
+        public bool DoSpreadInWinter
+        {
+            get => this._adapter.DoSpreadInWinter;
+            set => this._config.DoSpreadInWinter = value;
+        }
+
+        public bool DoGrowInstantly
+        {
+            get => this._adapter.DoGrowInstantly;
+            set => this._config.DoGrowInstantly = value;
+        }
+
+        public bool DoSeedsPersist
+        {
+            get => this._adapter.DoSeedsPersist;
+            set => this._config.DoSeedsPersist = value;
+        }
+
+        public bool DoMushroomTreesHibernate
+        {
+            get => this._adapter.DoMushroomTreesHibernate;
+            set => this._config.DoMushroomTreesHibernate = value;
+        }
+
+        public bool DoMushroomTreesRegrow
+        {
+            get => this._adapter.DoMushroomTreesRegrow;
+            set => this._config.DoMushroomTreesRegrow = value;
+        }
+
         public int MaxPassableGrowthStage
         {
-            get => this._maxPassableGrowthStage;
-            set => this._maxPassableGrowthStage = value;
+            get => this._adapter.MaxPassableGrowthStage;
+            set => this._config.MaxPassibleGrowthStage = value;
         }
 
-        public bool RollForSpread =>
-            this.SpreadRoller?.Invoke() ?? ConfigAdapter.RandomChance(this.DailySpreadChance);
+        public double DailyGrowthChance
+        {
+            set => this._config.DailyGrowthChance = value;
+        }
 
-        public bool RollForGrowth =>
-            this.GrowthRoller?.Invoke() ?? ConfigAdapter.RandomChance(this.DailyGrowthChance);
+        public double DailySpreadChance
+        {
+            set => this._config.DailySpreadChance = value;
+        }
 
-        public bool RollForSeed =>
-            this.SeedRoller?.Invoke() ?? ConfigAdapter.RandomChance(this.DailySeedChance);
+        public double DailySeedChance
+        {
+            set => this._config.DailySeedChance = value;
+        }
 
-        public bool RollForMushroomRegrowth =>
-            this.MushroomRegrowthRoller?.Invoke() ?? ConfigAdapter.RandomChance(this.DailyGrowthChance / 2);
+        public bool RollForSpread => this.SpreadRoller();
 
-        public Func<bool> SpreadRoller { private get; set; }
-        public Func<bool> GrowthRoller { private get; set; }
-        public Func<bool> SeedRoller { private get; set; }
-        public Func<bool> MushroomRegrowthRoller { private get; set; }
+        public bool RollForGrowth => this.GrowthRoller();
+
+        public bool RollForSeed => this.SeedRoller();
+
+        public bool RollForMushroomRegrowth => this.MushroomRegrowthRoller();
     }
 }
--- AggressiveAcorns/ConfigAdapter.cs
+++ AggressiveAcorns/ConfigAdapter.cs
@@ -4,49 +4,34 @@ namespace Phrasefable.StardewMods.AggressiveAcorns
 {
     internal class ConfigAdapter : IConfigAdapter
     {
-        private readonly double _dailySpreadChance;
-        private readonly double _dailyGrowthChance;
-        private readonly double _dailySeedChance;
+        private readonly IModConfig _base;
+
 
         public ConfigAdapter(IModConfig config)
         {
-            this.ProtectFromMelee = config.PreventScythe;
-            this.SeedsReplaceGrass = config.SeedsReplaceGrass;
-            this.MaxShadedGrowthStage = config.MaxShadedGrowthStage;
-            this.MaxPassableGrowthStage = config.MaxPassibleGrowthStage;
-            this.DoGrowInWinter = config.DoGrowInWinter;
-            this.DoTappedSpread = config.DoTappedSpread;
-            this.DoSpreadInWinter = config.DoSpreadInWinter;
-            this.DoGrowInstantly = config.DoGrowInstantly;
-            this.DoSeedsPersist = config.DoSeedsPersist;
-            this.DoMushroomTreesHibernate = config.DoMushroomTreesHibernate;
-            this.DoMushroomTreesRegrow = config.DoMushroomTreesRegrow;
-
-            this._dailySpreadChance = config.DailySpreadChance;
-            this._dailyGrowthChance = config.DailyGrowthChance;
-            this._dailySeedChance = config.DailySeedChance;
+            this._base = config;
         }
 
 
-        public bool ProtectFromMelee { get; }
-        public bool SeedsReplaceGrass { get; }
-        public int MaxShadedGrowthStage { get; }
-        public int MaxPassableGrowthStage { get; }
-        public bool DoGrowInWinter { get; }
-        public bool DoTappedSpread { get; }
-        public bool DoSpreadInWinter { get; }
-        public bool DoGrowInstantly { get; }
-        public bool DoSeedsPersist { get; }
-        public bool DoMushroomTreesHibernate { get; }
-        public bool DoMushroomTreesRegrow { get; }
+        public bool ProtectFromMelee => this._base.PreventScythe;
+        public bool SeedsReplaceGrass => this._base.SeedsReplaceGrass;
+        public int MaxShadedGrowthStage => this._base.MaxShadedGrowthStage;
+        public int MaxPassableGrowthStage => this._base.MaxPassibleGrowthStage;
+        public bool DoGrowInWinter => this._base.DoGrowInWinter;
+        public bool DoTappedSpread => this._base.DoTappedSpread;
+        public bool DoSpreadInWinter => this._base.DoSpreadInWinter;
+        public bool DoGrowInstantly => this._base.DoGrowInstantly;
+        public bool DoSeedsPersist => this._base.DoSeedsPersist;
+        public bool DoMushroomTreesHibernate => this._base.DoMushroomTreesHibernate;
+        public bool DoMushroomTreesRegrow => this._base.DoMushroomTreesRegrow;
 
-        public bool RollForSpread => ConfigAdapter.RandomChance(this._dailySpreadChance);
-        public bool RollForGrowth => ConfigAdapter.RandomChance(this._dailyGrowthChance);
-        public bool RollForSeed => ConfigAdapter.RandomChance(this._dailySeedChance);
-        public bool RollForMushroomRegrowth => ConfigAdapter.RandomChance(this._dailyGrowthChance / 2);
+        public bool RollForSpread => ConfigAdapter.RandomChance(this._base.DailySpreadChance);
+        public bool RollForGrowth => ConfigAdapter.RandomChance(this._base.DailyGrowthChance);
+        public bool RollForSeed => ConfigAdapter.RandomChance(this._base.DailySeedChance);
+        public bool RollForMushroomRegrowth => ConfigAdapter.RandomChance(this._base.DailyGrowthChance / 2);
 
 
-        internal static bool RandomChance(double chance)
+        private static bool RandomChance(double chance)
         {
             return Game1.random.NextDouble() < chance;
         }
