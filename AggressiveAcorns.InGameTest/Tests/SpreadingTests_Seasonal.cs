a63c50519119c4ed8fca2127632d7adbf6bacfad
Phrasefable <12666934+phrasefable@users.noreply.github.com>
2021-03-02 18:39:16 +1300

    AA.IGT: Add tests for seasonal spreading

---
+++ AggressiveAcorns.InGameTest/Tests/SpreadingTests_Seasonal.cs
@@ -0,0 +1,90 @@
+using Phrasefable.StardewMods.AggressiveAcorns.InGameTest.Utilities;
+using Phrasefable.StardewMods.StarUnit.Framework.Builders;
+using Phrasefable.StardewMods.StarUnit.Framework.Definitions;
+using Phrasefable.StardewMods.StarUnit.Framework.Results;
+using StardewValley;
+
+namespace Phrasefable.StardewMods.AggressiveAcorns.InGameTest.Tests
+{
+    internal partial class SpreadingTests
+    {
+        private ITraversable BuildFixture_Seasonal()
+        {
+            string initialSeason = null;
+
+            ITestFixtureBuilder builder = this._factory.CreateFixtureBuilder();
+
+            builder.Key = "spreading_seasonal";
+
+            // Not needed while sub-fixture of SpreadingTests:
+            // builder.AddCondition(this._factory.Conditions.WorldReady);
+            // fixtureBuilder.BeforeAll = () => Game1.player.warpFarmer(Utils.WarpFarm);
+
+            builder.BeforeAll = () => initialSeason = Game1.currentSeason;
+            builder.BeforeAllDelay = Delay.Second;
+            builder.AfterAll = () => SeasonUtils.SetSeason(initialSeason);
+
+            builder.BeforeEach = () =>
+            {
+                this._config = new MutableConfigAdaptor();
+                AggressiveAcorns.Config = this._config;
+
+                this._seedLocator = new SeedLocator();
+                this._config.SpreadOffsetGenerator = () => this._seedLocator.GenerateOffsets();
+            };
+
+            builder.Delay = Delay.Tick;
+
+            builder.AddChild(this.BuildTest_SeasonalSpreading());
+
+            return builder.Build();
+        }
+
+
+        // ========== Seasonal Spread  ========================================================================
+
+        private ITraversable BuildTest_SeasonalSpreading()
+        {
+            ICasedTestBuilder<(Season Season, bool AllowWinterSpread, bool ExpectSpread)> testBuilder =
+                this._factory.CreateCasedTestBuilder<(Season, bool, bool)>();
+
+            testBuilder.Key = "seasonal_spreading";
+            testBuilder.TestMethod = this.Test_SeasonalSpreading;
+            testBuilder.Delay = Delay.Tick;
+            testBuilder.KeyGenerator = args =>
+                $"{args.Season.GetName()}_{(args.AllowWinterSpread ? "with" : "without")}_winter_growth";
+            testBuilder.AddCases(
+                (Season: Season.Spring, false, true),
+                (Season: Season.Summer, false, true),
+                (Season: Season.Fall, false, true),
+                (Season: Season.Winter, false, false)
+            );
+            testBuilder.AddCases(
+                (Season: Season.Spring, true, true),
+                (Season: Season.Summer, true, true),
+                (Season: Season.Fall, true, true),
+                (Season: Season.Winter, true, true)
+            );
+
+            return testBuilder.Build();
+        }
+
+
+        private ITestResult Test_SeasonalSpreading((Season, bool, bool) args)
+        {
+            (Season season, bool allowWinterSpread, bool expectSpread) = args;
+
+            // Arrange
+            season.SetSeason();
+            this._config.DailySpreadChance = 1.0;
+            this._config.DoSpreadInWinter = allowWinterSpread;
+
+            // Act, Assert
+            ITestResult result = this.UpdateAndCheckTreeHasSpread(
+                Utilities.TreeUtils.GetFarmTreeLonely(),
+                expectSpread
+            );
+            return result;
+        }
+    }
+}
---
+++ AggressiveAcorns.InGameTest/Utilities/SeasonUtils.cs
@@ -0,0 +1,44 @@
+using System;
+using StardewValley;
+
+namespace Phrasefable.StardewMods.AggressiveAcorns.InGameTest.Utilities
+{
+    internal static class SeasonUtils
+    {
+        public static void SetSeason(string seasonName)
+        {
+            Game1.currentSeason = seasonName;
+            Game1.setGraphicsForSeason();
+        }
+    }
+
+
+    internal enum Season
+    {
+        Spring,
+        Summer,
+        Fall,
+        Winter
+    }
+
+
+    internal static class SeasonsExtensions
+    {
+        public static string GetName(this Season season)
+        {
+            return season switch
+            {
+                Season.Spring => "spring",
+                Season.Summer => "summer",
+                Season.Fall => "fall",
+                Season.Winter => "winter",
+                _ => throw new ArgumentOutOfRangeException(nameof(season), season, null)
+            };
+        }
+
+        public static void SetSeason(this Season season)
+        {
+            SeasonUtils.SetSeason(season.GetName());
+        }
+    }
+}
