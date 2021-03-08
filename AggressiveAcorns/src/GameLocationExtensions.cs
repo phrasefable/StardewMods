f6320038fc64b34547607313cb63fb6499a79bf8
phrasefable <12666934+phrasefable@users.noreply.github.com>
2021-03-08 19:25:04 +1300

    AA: Move game location extension methods out of TreeUtils

---
+++ AggressiveAcorns/src/GameLocationExtensions.cs
@@ -0,0 +1,52 @@
+using JetBrains.Annotations;
+using Microsoft.Xna.Framework;
+using StardewValley;
+using StardewValley.Locations;
+using StardewValley.TerrainFeatures;
+
+namespace Phrasefable.StardewMods.AggressiveAcorns
+{
+    internal static class GameLocationExtensions
+    {
+        [Pure]
+        public static bool TreeCanGrowAt(this GameLocation location, Tree tree, Vector2 position)
+        {
+            string prop = location.doesTileHaveProperty((int) position.X, (int) position.Y, "NoSpawn", "Back");
+            bool tileCanSpawnTree = prop == null || !(prop.Equals("All") || prop.Equals("Tree") || prop.Equals("True"));
+            bool isBlockedSeed = tree.growthStage.Value == 0 && location.objects.ContainsKey(position);
+            return tileCanSpawnTree && !isBlockedSeed;
+        }
+
+
+        [Pure]
+        public static bool ExperiencingWinter(this GameLocation location)
+        {
+            return Game1.IsWinter && location.ExperiencesWinter();
+        }
+
+
+        [Pure]
+        public static bool ExperiencesWinter(this GameLocation location)
+        {
+            return location.IsOutdoors && !(location is Desert);
+        }
+
+
+        [Pure]
+        public static bool IsShadedAt(this GameLocation location, Vector2 position)
+        {
+            foreach (Vector2 adjacentTile in Utility.getSurroundingTileLocationsArray(position))
+            {
+                if (location.terrainFeatures.TryGetValue(adjacentTile, out TerrainFeature feature)
+                    && feature is Tree adjTree
+                    && adjTree.IsFullyGrown()
+                    && !adjTree.stump.Value)
+                {
+                    return true;
+                }
+            }
+
+            return false;
+        }
+    }
+}
--- AggressiveAcorns/src/TreeUtils.cs
+++ AggressiveAcorns/src/TreeUtils.cs
@@ -1,8 +1,6 @@
 using System.Collections.Generic;
 using Microsoft.Xna.Framework;
 using StardewValley;
-using StardewValley.Locations;
-using StardewValley.TerrainFeatures;
 
 namespace Phrasefable.StardewMods.AggressiveAcorns
 {
@@ -14,44 +12,6 @@ namespace Phrasefable.StardewMods.AggressiveAcorns
 
             static int GetOffset() => Game1.random.Next(-3, 4);
             yield return new Vector2(GetOffset(), GetOffset());
-        }
-
-
-        public static bool TreeCanGrowAt(this GameLocation location, Tree tree, Vector2 position)
-        {
-            string prop = location.doesTileHaveProperty((int) position.X, (int) position.Y, "NoSpawn", "Back");
-            bool tileCanSpawnTree = prop == null || !(prop.Equals("All") || prop.Equals("Tree") || prop.Equals("True"));
-            bool isBlockedSeed = tree.growthStage.Value == 0 && location.objects.ContainsKey(position);
-            return tileCanSpawnTree && !isBlockedSeed;
-        }
-
-
-        public static bool ExperiencingWinter(this GameLocation location)
-        {
-            return Game1.IsWinter && location.ExperiencesWinter();
-        }
-
-
-        public static bool ExperiencesWinter(this GameLocation location)
-        {
-            return location.IsOutdoors && !(location is Desert);
-        }
-
-
-        public static bool IsShadedAt(this GameLocation location, Vector2 position)
-        {
-            foreach (Vector2 adjacentTile in Utility.getSurroundingTileLocationsArray(position))
-            {
-                if (location.terrainFeatures.TryGetValue(adjacentTile, out TerrainFeature feature)
-                    && feature is Tree adjTree
-                    && adjTree.IsFullyGrown()
-                    && !adjTree.stump.Value)
-                {
-                    return true;
-                }
-            }
-
-            return false;
         }
     }
 }
