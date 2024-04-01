0f01bd5067eb0a661ab52b3556535ea80352d6ad
phrasefable <12666934+phrasefable@users.noreply.github.com>
2024-04-01 13:22:32 +1300

    AA: Add informational entries to config

--- AggressiveAcorns/AggressiveAcorns.cs
+++ AggressiveAcorns/AggressiveAcorns.cs
@@ -26,6 +26,8 @@ namespace Phrasefable.StardewMods.AggressiveAcorns
 
         private void GameLoop_GameLaunched(object sender, StardewModdingAPI.Events.GameLaunchedEventArgs e)
         {
+            Config.ResetInfoEntries();
+            this.Helper.WriteConfig(Config);
             this.SetupGenericConfigMenu();
         }
 
--- AggressiveAcorns/ModConfig.cs
+++ AggressiveAcorns/ModConfig.cs
@@ -4,6 +4,7 @@ namespace Phrasefable.StardewMods.AggressiveAcorns
 {
     public class ModConfig : IModConfig
     {
+
         public bool DoMeleeWeaponsDestroySeedlings { get; set; } = false;
 
         public int MaxPassableGrowthStage { get; set; } = Tree.seedStage;
@@ -25,6 +26,75 @@ namespace Phrasefable.StardewMods.AggressiveAcorns
 
         //public bool DoMushroomTreesHibernate { get; set; } = true;
         //public bool DoMushroomTreesRegrow { get; set; } = false;
+
+
+        // ===============================================================================
+        public string Info_INFO { get; private set; }
+        public Dictionary<object, object> GrowthStages_INFO { get; private set; }
+        public Dictionary<object, object> TreeTypes_INFO { get; private set; }
+
+        // ===============================================================================
+
+
+        public ModConfig()
+        {
+            this.ResetInfoEntries();
+        }
+
+        public void ResetInfoEntries()
+        {
+            this.Info_INFO = "All entries ending with '_INFO' will be reset each time the game is launched";
+            this.GrowthStages_INFO = new Dictionary<object, object>
+            {
+                 {"Description", "Stage ID (integer) - use in config options"},
+                 {"Seed", Tree.seedStage},
+                 {"Sprout", Tree.sproutStage},
+                 {"Sapling", Tree.saplingStage},
+                 {"Bush", Tree.bushStage},
+                 {"Still a bush", Tree.bushStage + 1},
+                 {"Tree", Tree.treeStage},
+                 {"Old enough to grow moss", Tree.stageForMossGrowth},
+                 {"Maximum", Tree.stageForMossGrowth + 1},
+            };
+
+            string[] vanillaTreeTypes = [
+                Tree.bushyTree, Tree.leafyTree, Tree.pineTree,
+                Tree.winterTree1, Tree.winterTree2,
+                Tree.palmTree, Tree.mushroomTree, Tree.mahoganyTree, Tree.palmTree2,
+                Tree.greenRainTreeBushy, Tree.greenRainTreeLeafy, Tree.greenRainTreeFern, Tree.mysticTree
+            ];
+
+            this.TreeTypes_INFO = new Dictionary<object, object>
+            {
+                {"Tree type ID (string) - use in config options", ""},
+                {Tree.bushyTree, "Oak"},
+                {Tree.leafyTree, "Maple"},
+                {Tree.pineTree, "Pine"},
+                {Tree.palmTree, "Palm - Desert"},
+                {Tree.mushroomTree, "Mushroom"},
+                {Tree.mahoganyTree, "Mahogany"},
+                {Tree.palmTree2, "Palm - Ginger Is."},
+                {Tree.greenRainTreeBushy, "Green rain tree - transforms from Oak"},
+                {Tree.greenRainTreeLeafy, "Green rain tree - transforms from Maple"},
+                {Tree.greenRainTreeFern, "Green rain tree fern"},
+                {Tree.mysticTree, "Mystic"},
+            };
+
+            Dictionary<string, StardewValley.GameData.WildTrees.WildTreeData> data = null;
+            try
+            {
+                data = Tree.GetWildTreeDataDictionary();
+            }
+            catch (NullReferenceException) { }
+            if (data is not null)
+            {
+                foreach (string key in data.Keys.Except(vanillaTreeTypes))
+                {
+                    this.TreeTypes_INFO.Add(key, "(modded)");
+                }
+            }
+
+        }
     }
 
 
